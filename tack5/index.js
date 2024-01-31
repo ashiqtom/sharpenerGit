3167feddb48e0afabf9063adce994e04985cfb09
ashiqtom <ashiqtom^@gmail.com>
2024-01-31 15:20:25 +0530

    use getElementsByTagName

---
+++ tack5/index.js
@@ -0,0 +1,7 @@
+// Write your code below:
+const listElement=document.getElementsByTagName("li")
+listElement[4].style.color="blue"
+
+for(let i=0;i<listElement.length;i++){
+  listElement[i].style.fontStyle="italic"
+}
