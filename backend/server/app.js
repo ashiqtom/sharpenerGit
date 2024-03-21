dc38e6a167cc08cb88e547061ead4e259b86aa2e
ashiqtom <ashiqtom^@gmail.com>
2024-03-21 16:04:42 +0530

    add Request and Response

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,7 +1,23 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
-    console.log("Ashiq");
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title><head>')
+
+    if (req.url === '/home') {
+        res.write("<body><h1>Welcome home</h1></body>");
+    } else if (req.url === '/about') {
+        res.write("<body><h1>Welcome to About Us page</h1></body>");
+    } else if (req.url === '/node') {
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    } else {
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>")
+    }
+
+    res.write('</html>')
+    res.end()
 })
-server.listen(4000)
-
+server.listen(4000);
