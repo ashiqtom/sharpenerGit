b09602611d9f545279bf6b418820baafaffed363
ashiqtom <ashiqtom^@gmail.com>
2024-04-01 08:10:54 +0530

    Lets get started with Express

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,10 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-//const server = http.createServer(routes);
+app.use((req,res,next)=>{
+    console.log('In the middleware!');
+    next();
+});
 
-const server = http.createServer(routes.handler);
-console.log(routes.someText)
+app.use((req,res,next)=>{
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(4000);
+app.listen(4000)
