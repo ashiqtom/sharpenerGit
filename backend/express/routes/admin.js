5da7159975a0d3a733b721b5aa5623aa005a6217
ashiqtom <ashiqtom^@gmail.com>
2024-04-05 16:38:17 +0530

    05 Serving HTML , CSS and JS (VIEWS)

--- backend/express/routes/admin.js
+++ backend/express/routes/admin.js
@@ -1,14 +1,16 @@
 const express=require('express');
 
 const router=express.Router();
+const path=require('path');
+const rootDir=require('../util/path');
 
 //admin/add-product -with GET 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 //admin/add-product -with POST
-router.post('/product',(req,res,next) => { 
+router.post('/add-product',(req,res,next) => { 
     console.log(req.body)
     res.redirect("/")
 });
