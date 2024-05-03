4e6cbd024699cd9bb683851930cc184317aef310
ashiqtom <ashiqtom^@gmail.com>
2024-05-03 13:56:59 +0530

    Your user's password is at risk!!!!

--- backend/Expense Tracker - Node.js Project/backend/controllers/admin.js
+++ backend/Expense Tracker - Node.js Project/backend/controllers/admin.js
@@ -1,18 +1,24 @@
 
 const User = require('../models/user');
+const bcrypt=require('bcrypt');
 
 exports.postUser=async (req, res) => {
     try {
         const { username, email, password } = req.body;
 
+        
+
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).json({ error: 'Email already exists' });
         }
+        bcrypt.hash(password,10,async(err,hash)=>{
+            console.log(err);
+            const newUser = await User.create({ username, email, password:hash});
+            res.status(201).json(newUser);
+        })
 
-        const newUser = await User.create({ username, email, password });
-
-        res.status(201).json(newUser);
+        
     } catch (error) {
         console.error('Error signing up:', error);
         res.status(500).json({ error: 'Internal server error' });
@@ -26,12 +32,14 @@ exports.getlogin = async (req, res) => {
         if (!existingUser) {
             return res.status(404).json({ message: 'Invalid email' });
         }
-        const existingPassword = existingUser.password; 
-        if (existingPassword !== password) {
+        const existingPassword = existingUser.password;
+        
+        const passwordCompared=await bcrypt.compare(password,existingPassword);
+        if(passwordCompared){
+            return res.status(200).json({ message: 'Login successful' });
+        }else{
             return res.status(401).json({ message: 'Invalid password' });
         }
-        return res.status(200).json({ message: 'Login successful' });
-        
     } catch (error) {
         console.error('Error login:', error);
         return res.status(500).json({ message: 'Internal server error' });
